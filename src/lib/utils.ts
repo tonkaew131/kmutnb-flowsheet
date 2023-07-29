8c65ade769710ace4dcb5d81ca7ab98924d73207
tonkaew131 <nongtonkaew@gmail.com>
2023-07-29 22:59:29 +0700

    Fixed CommonJS Module iconv-lite

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
-import { decode, encode } from 'iconv-lite';
+import iconv from 'iconv-lite';
 import { xml2json } from 'xml-js';
 
 export function convertTISToUTF8(tis620Text: string | ArrayBuffer): string {
-	const buffer = decode(Buffer.from(tis620Text, 'binary'), 'tis620');
-	const utf8Text = encode(buffer, 'utf-8').toString();
+	const buffer = iconv.decode(Buffer.from(tis620Text, 'binary'), 'tis620');
+	const utf8Text = iconv.encode(buffer, 'utf-8').toString();
 
 	return utf8Text;
 }
