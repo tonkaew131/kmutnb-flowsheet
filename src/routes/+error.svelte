d4eea2feab315b226a8e1abe09b5f06d5765e49c
tonkaew131 <nongtonkaew@gmail.com>
2023-07-29 21:43:21 +0700

    Added Error Page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+
+	import '../tailwind.postcss';
+</script>
+
+<div class="text-4xl text-center h-screen flex flex-col items-center justify-center font-noto font-bold text-primary-500">
+	<h1 class="">
+		{$page?.error?.message || 'Something went wrong! Please contact @tonkaew131'}
+	</h1>
+    <h2 class="font-black text-black tracking-widest">{$page.status}</h2>
+</div>
