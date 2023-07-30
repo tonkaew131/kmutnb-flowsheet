ddac885112b9c8cfaa7c4c119a7e7bf6d9a72d3c
tonkaew131 <nongtonkaew@gmail.com>
2023-07-30 16:47:50 +0700

    Added 404 error when no data is found

--- src/routes/c/[courseId]/+page.server.ts
+++ src/routes/c/[courseId]/+page.server.ts
@@ -1,9 +1,10 @@
 import { ENV_TYPE } from '$env/static/private';
 
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
-import type { CurriculumData } from '$lib/types/curriculum';
 import { convertTISToUTF8, convertXMLToJSON } from '$lib/utils';
+import type { CurriculumData } from '$lib/types/curriculum';
 
 const KMUTNB_API = 'http://klogic.kmutnb.ac.th:8080/kris/curri/showXML.jsp?currCode=';
 
@@ -13,35 +14,35 @@ export const load = (async ({ params }) => {
 	let response;
 	if (ENV_TYPE === 'development') {
 		console.log('DEV MODE: Using local XML file');
-		response = await fetch(`http://localhost:5173/CS 64046034_raw.xml`);
+		response = await fetch(`http://localhost:5173/showXML.jsp`);
 	} else {
 		try {
 			response = await fetch(`${KMUTNB_API}${courseId}`);
-		} catch (error) {
-			return {
-				status: 'error',
-				error: {
-					message: 'ไม่สามารถดึงข้อมูลจาก klogic ได้ (ลองใหม่ๆๆ)'
-				}
-			};
+		} catch (errMsg) {
+			throw error(500, {
+				message: 'ไม่สามารถดึงข้อมูลจาก klogic ได้ (ลองใหม่ๆๆ)'
+			});
 		}
 	}
 
 	const xml = await response.arrayBuffer();
 	const utf8XML = convertTISToUTF8(xml);
 
+	if (xml.byteLength == 5) {
+		throw error(404, {
+			message: 'ไม่พบข้อมูลรายวิชาที่ร้องขอ'
+		});
+	}
+
 	let json: CurriculumData;
 	try {
 		json = JSON.parse(convertXMLToJSON(utf8XML));
-	} catch (error) {
-		console.error(error);
+	} catch (errMsg) {
+		console.error(errMsg);
 
-		return {
-			status: 'error',
-			error: {
-				message: 'ไม่สามารถแปลงข้อมูล XML ได้ (ติดต่อผู้ดูแลระบบ)'
-			}
-		};
+		throw error(500, {
+			message: 'ไม่สามารถแปลงข้อมูล XML ได้ (ติดต่อผู้ดูแลระบบ)'
+		});
 	}
 
 	return { status: 'success', data: json, courseId: courseId };
