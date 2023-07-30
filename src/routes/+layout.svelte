e3eb6850a29f43534057186be6f58ce8c6e37190
tonkaew131 <nongtonkaew@gmail.com>
2023-07-30 12:29:50 +0700

    Added SEO a bit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,23 @@
 	import Analytics from '$lib/analytics.svelte';
 </script>
 
+<svelte:head>
+	<title>KMUTNB Flowsheet by @tonkaew131</title>
+	<meta name="description" content="KMUTNB Flowsheet ดูแผนการเรียน by @tonkaew131" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="theme-color" content="#f77100" />
+
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/favicon.png" />
+	<!-- <link rel="manifest" href="/manifest.json" /> -->
+
+	<meta property="og:title" content="KMUTNB Flowsheet by @tonkaew131" />
+	<meta property="og:description" content="KMUTNB Flowsheet by @tonkaew131" />
+	<meta property="og:image" content="/favicon.png" />
+	<meta property="og:url" content="https://kmutnb-flowsheet.vercel.app/" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <Analytics />
 <AppShell>
 	<slot />
