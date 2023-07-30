ba79378984dd6c27da9dc17004777768fbe8ee89
tonkaew131 <nongtonkaew@gmail.com>
2023-07-30 23:10:01 +0700

    Added features to dropdown select curriculums

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,49 @@
 
 		goto(`/c/${courseId}`);
 	}
+
+	let departments: {
+		[key: string]: {
+			name: string;
+			curriculums: {
+				[key: string]: {
+					name: string;
+				};
+			};
+		};
+	};
+	function onSelectFaculty(e: any) {
+		const faculty = e?.target?.value;
+
+		departments = courseList[faculty]?.departments;
+		console.log(departments);
+	}
+
+	let curriculums: {
+		[key: string]: {
+			name: string;
+		};
+	};
+	function onSelectDepartment(e: any) {
+		const department = e?.target?.value;
+
+		curriculums = departments[department]?.curriculums;
+		console.log(curriculums);
+	}
+
+	let currCode: string;
+	function onSelectCurriculum(e: any) {
+		const curriculum = e?.target?.value;
+
+		currCode = curriculum;
+	}
 </script>
 
 <div class="container h-full mx-auto flex flex-col justify-center items-center font-noto">
 	<form on:submit|preventDefault={onSubmit}>
 		<label>
 			<span class="font-bold">คณะ</span>
-			<select class="select">
+			<select class="select" on:change={onSelectFaculty}>
 				<option value="NONE" selected disabled>- - - เลือกคณะ - - -</option>
 				{#each Object.keys(courseList).sort() as c}
 					<option value={c}>{courseList[c].name}</option>
@@ -21,7 +57,31 @@
 			</select>
 		</label>
 
-		<div class="relative">
+		{#if departments}
+			<label>
+				<span class="font-bold">ภาควิชา</span>
+				<select class="select" on:change={onSelectDepartment}>
+					<option value="NONE" selected disabled>- - - เลือกภาควิชา - - -</option>
+					{#each Object.keys(departments).sort() as d}
+						<option value={d}>{departments[d].name}</option>
+					{/each}
+				</select>
+			</label>
+		{/if}
+
+		{#if curriculums}
+			<label>
+				<span class="font-bold">ภาควิชา</span>
+				<select class="select" on:change={onSelectCurriculum}>
+					<option value="NONE" selected disabled>- - - เลือกภาควิชา - - -</option>
+					{#each Object.keys(curriculums) as c}
+						<option value={c}>{curriculums[c].name}</option>
+					{/each}
+				</select>
+			</label>
+		{/if}
+
+		<div class="relative mt-3">
 			<div
 				class="h-[2px] -z-10 w-full bg-primary-500 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
 			/>
@@ -30,7 +90,13 @@
 
 		<label>
 			<span class="font-bold">รหัสหลักสูตร</span>
-			<input class="input px-4 py-3" type="text" name="courseId" placeholder="64046034" />
+			<input
+				class="input px-4 py-3"
+				type="text"
+				name="courseId"
+				bind:value={currCode}
+				placeholder="64046034"
+			/>
 		</label>
 
 		<button type="submit" class="btn variant-ghost-primary w-full font-bold tracking-widest mt-4"
